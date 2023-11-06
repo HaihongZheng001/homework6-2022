685ec4d60fd0b95626c1bbb1b7e362f46ccac646
HaihongZheng001 <89872150+HaihongZheng001@users.noreply.github.com>
2023-11-06 13:59:48 -0500

    Add files via upload

--- video.js
+++ video.js
@@ -11,7 +11,7 @@ window.addEventListener("load", function() {
 	video.loop = false;
 	video.volume = document.querySelector("#slider").value / 100;
 
-	// Play button
+	// Play button + volume
 	document.querySelector("#play").addEventListener("click", function() {
 		console.log("Play Video");
 		video.play();
@@ -27,44 +27,51 @@ window.addEventListener("load", function() {
 
 	// Slow down
 	document.querySelector("#slower").addEventListener("click", function() {
-		video.playbackRate *= 0.95;
+		video.playbackRate *= 0.9;
 		console.log("New speed is" + video.playbackRate);
 	});
 
 	// Speed up
 	document.querySelector("#faster").addEventListener("click", function() {
-		video.playbackRate /= 0.95;
+		video.playbackRate /= 0.9;
 		console.log("New speed is" + video.playbackRate);
 	})
 
 
 	// Skip ahead
 	document.querySelector("#skip").addEventListener("click", function() {
-		video.currentTime += 15;
-		if (video.currentTime >= video.duration) {
+		
+		if (video.currentTime + 10 >= video.duration) {
 			video.currentTime = 0;
+		} else {
+			video.currentTime += 10;
 		}
 		console.log("Current video time is" + video.currentTime);
 	})
 
-	// Mute
+	// Mute & unmute
 	document.querySelector("#mute").addEventListener("click", function() {
-		if (video.muted == false) {
-			video.muted = true;
-			this.innerHTML = "Unmute";
-		} else {
+		if (video.muted) {
 			video.muted = false;
-			this.innerHTML = "Mute";
+			this.textContent = "Mute";
+		} else {
+			video.muted = true;
+			this.textContent = "Unmute";
 		}
 	})
 
 
 	// Volume slider
-	document.querySelector("#slider").addEventListener("click", function() {
-		console.log(this.value);
-		video.volume = this.value/100;
-		document.querySelector("#volume").innerHTML = video.volume*100 + "%";
-	})
+	// document.querySelector("#slider").addEventListener("click", function() {
+	// 	console.log(this.value);
+	// 	video.volume = this.value/100;
+	// 	document.querySelector("#volume").innerHTML = video.volume*100 + "%";
+	// })
+
+	document.querySelector("#slider").addEventListener("input", function() {
+		video.volume = this.value / 100;
+		document.querySelector("#volume").textContent = this.value + "%";
+	});
 
 	// Styled
 	document.querySelector("#vintage").addEventListener("click", function() {
